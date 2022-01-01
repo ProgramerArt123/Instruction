017b104cbaaafe155c64f1ccc535551073e41204
ProgramerArt123 <2039231428@qq.com>
2022-01-02 00:07:04 +0800

    Offset32

---
+++ x86/register/offset/Offset32.cpp
@@ -0,0 +1,13 @@
+#include "Offset32.h"
+namespace x86
+{
+	namespace reg
+	{
+		namespace offset
+		{
+			Offset32::Offset32() {
+		
+			}
+		}
+	}
+}
---
+++ x86/register/offset/Offset32.h
@@ -0,0 +1,19 @@
+#ifndef __X86_REGISTER_OFFSET_32_H__
+#define __X86_REGISTER_OFFSET_32_H__
+#include "Offset.h"
+namespace x86
+{
+	namespace reg
+	{
+		namespace offset
+		{
+			class Offset32 : public Offset {
+			public:
+				explicit Offset32();
+
+			};
+		}
+	}
+}
+
+#endif
