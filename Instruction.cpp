4e3d25da4c1175a5ac2eb508dd3a775d221445f3
ProgramerArt123 <2039231428@qq.com>
2022-01-01 19:37:24 +0800

    use accumulator

--- Instruction.cpp
+++ Instruction.cpp
@@ -1,8 +1,10 @@
+#include "Define.h"
+#include "Operand.h"
 #include "Instruction.h"
 
 Instruction::Instruction(const char *mnemonic): 
 	m_mnemonic(mnemonic) {
-	
+		m_string = m_mnemonic;
 }
 Instruction::Instruction(const char *mnemonic, Operand &source): 
 	m_mnemonic(mnemonic), m_source(&source) {
@@ -10,7 +12,11 @@ Instruction::Instruction(const char *mnemonic, Operand &source):
 }
 Instruction::Instruction(const char *mnemonic, Operand &source, Operand &destination): 
 	m_mnemonic(mnemonic), m_source(&source), m_destination(&destination) {
-	
+#if FORMAT=='A'
+		m_string = m_mnemonic + "\t" + m_source->ToString() + ", " + m_destination->ToString();
+#else
+		m_string = m_mnemonic + "\t" + m_destination->ToString() + ", " + m_source->ToString();
+#endif
 }
 Instruction::Instruction(const char *mnemonic, Operand &source, Operand &destination, Operand &auxiliary): 
 	m_mnemonic(mnemonic), m_source(&source), m_destination(&destination), m_auxiliary(&auxiliary) {
@@ -19,13 +25,10 @@ Instruction::Instruction(const char *mnemonic, Operand &source, Operand &destina
 Instruction::~Instruction() {
 	
 }
-const char *Instruction::ToATATString() {
-	return m_mnemonic.c_str();
-}
-const char *Instruction::ToIntelString() {
-	return m_mnemonic.c_str();
+const char *Instruction::String() {
+	return m_string.c_str();
 }
 std::ostream &operator<<(std::ostream &out, Instruction &instruction) {
-	out << instruction.ToATATString();
+	out << instruction.String();
 	return out;
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
 #include "Instruction.h"
+#include "Accumulator.h"
+#include "Base.h"
 int main(int argc, char *argv[])
 {
 	Instruction nop("NOP");
 	std::cout << nop << std::endl;
+	Instruction move("MOV", Accumulator::GetInstance(), Base::GetInstance());
+	std::cout << move << std::endl;
 	return 0;
 }
