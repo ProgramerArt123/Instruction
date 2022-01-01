8cd8ea5c97a7f9df2afebfc97cbcefcc270046c8
ProgramerArt123 <2039231428@qq.com>
2022-01-01 22:12:19 +0800

    x86 Float Compare

---
+++ x86/FloatCompare.cpp
@@ -0,0 +1,32 @@
+#include "FloatCompare.h"
+const char *FloatCompare::Compare_ST__Value_4__8_C1_C0_C2_C3()const {
+	return "FCOM";
+}
+const char *FloatCompare::Compare_ST_Value_C1()const {
+	return "FCOMI";
+}
+const char *FloatCompare::Compare_Pop_Stack_ST_Value_C1()const {
+	return "FCOMIP";
+}
+const char *FloatCompare::Compare_Integer_2__4_C1_C0_C2_C3()const {
+	return "FICOM";
+}
+const char *FloatCompare::Compare_Pop_Stack_Integer_2__4_C1_C0_C2_C3()const {
+	return "FICOMP";
+}
+const char *FloatCompare::Compare_ZERO_C1_C0_C2_C3()const {
+	return "FTST";
+}
+const char *FloatCompare::Unordered_Compare_ST_C1_C0_C2_C3()const {
+	return "FUCOM";
+}
+const char *FloatCompare::Unordered_Compare_Pop_Stack_ST_C1_C0_C2_C3()const {
+	return "FUCOMP";
+}
+
+const char *FloatCompare::Unordered_Compare_Pop_Stack_2_ST_C1_C0_C2_C3()const {
+	return "FUCOMPP";
+}
+const char *FloatCompare::Examine_Value_C1_C0_C2_C3()const {
+	return "FXAM";
+}
---
+++ x86/FloatCompare.h
@@ -0,0 +1,18 @@
+#ifndef __FLOAT_COMPARE_H__
+#define __FLOAT_COMPARE_H__
+
+class FloatCompare {
+public:
+	const char *Compare_ST__Value_4__8_C1_C0_C2_C3()const;
+	const char *Compare_ST_Value_C1()const;
+	const char *Compare_Pop_Stack_ST_Value_C1()const;
+	const char *Compare_Integer_2__4_C1_C0_C2_C3()const;
+	const char *Compare_Pop_Stack_Integer_2__4_C1_C0_C2_C3()const;
+	const char *Compare_ZERO_C1_C0_C2_C3()const;
+	const char *Unordered_Compare_ST_C1_C0_C2_C3()const;
+	const char *Unordered_Compare_Pop_Stack_ST_C1_C0_C2_C3()const;
+	const char *Unordered_Compare_Pop_Stack_2_ST_C1_C0_C2_C3()const;
+	const char *Examine_Value_C1_C0_C2_C3()const;
+};
+
+#endif
