6b2f7cb15b9f6e6ac0356d1a31ab2833e773c5f8
ProgramerArt123 <2039231428@qq.com>
2022-01-01 20:18:29 +0800

    ConcreteInstruction64

---
+++ 64/ConcreteInstruction64.cpp
@@ -0,0 +1,5 @@
+#include "ConcreteInstruction64.h"
+namespace size64
+{
+		 
+}
---
+++ 64/ConcreteInstruction64.h
@@ -0,0 +1,8 @@
+#ifndef __CONCRETE_INSTRUCTION_64_H__
+#define __CONCRETE_INSTRUCTION_64_H__
+#include "../AbstractInstruction.h"
+namespace size64
+{
+		 
+}
+#endif
