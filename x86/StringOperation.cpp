17588f407e540ab28788d8f205c30a52a23b2362
ProgramerArt123 <2039231428@qq.com>
2022-01-02 22:49:39 +0800

    x86 string operation

---
+++ x86/StringOperation.cpp
@@ -0,0 +1,62 @@
+#include "StringOperation.h"
+
+const char *StringOperation::Transmitted_8()const {
+	return "MOVSB";
+}
+const char *StringOperation::Transmitted_16()const {
+	return "MOVSW";
+}
+const char *StringOperation::Transmitted_32()const {
+	return "MOVSD";
+}
+const char *StringOperation::Compare_Bytes_8_CF_OF_SF_ZF_AF_PF()const {
+	return "CMPSB";
+}
+const char *StringOperation::Compare_Bytes_16_CF_OF_SF_ZF_AF_PF()const {
+	return "CMPSW";
+}
+const char *StringOperation::Compare_Bytes_32_CF_OF_SF_ZF_AF_PF()const {
+	return "CMPSD";
+}
+const char *StringOperation::Compare_AX_Bytes_8_CF_OF_SF_ZF_AF_PF()const {
+	return "SCASB";
+}
+const char *StringOperation::Compare_AX_Bytes_16_CF_OF_SF_ZF_AF_PF()const {
+	return "SCASW";
+}
+const char *StringOperation::Compare_AX_Bytes_32_CF_OF_SF_ZF_AF_PF()const {
+	return "SCASD";
+}
+const char *StringOperation::Load_8_To_AL_From_DS_ESI()const {
+	return "LODSB";
+}
+const char *StringOperation::Load_16_To_AX_From_DS_ESI()const {
+	return "LODSW";
+}
+const char *StringOperation::Load_32_To_EAX_From_DS_ESI()const {
+	return "LODSD";
+}
+const char *StringOperation::Store_8_To_ES_EDI_From_AL()const {
+	return "STOSB";
+}
+const char *StringOperation::Store_16_To_ES_EDI_From_AX()const {
+	return "STOSW";
+}
+const char *StringOperation::Store_32_To_ES_EDI_From_EAX()const {
+	return "STOSD";
+}
+const char *StringOperation::Repeat_Prefix_ECX()const {
+	return "REP";
+}
+const char *StringOperation::Repeat_Prefix_EQUAL__ZF_ECX()const {
+	return "REPE";
+}
+const char *StringOperation::Repeat_Prefix_NOT_EQUAL__ZF_ECX()const {
+	return "REPNE";
+}
+const char *StringOperation::Repeat_Prefix_CF_ECX()const {
+	return "REPC";
+}
+const char *StringOperation::Repeat_Prefix_NOT_CF_ECX()const {
+	return "REPNC";
+}
---
+++ x86/StringOperation.h
@@ -0,0 +1,28 @@
+#ifndef __STRING_OPERATION_H__
+#define __STRING_OPERATION_H__
+
+class StringOperation {
+public:
+	const char *Transmitted_8()const;
+	const char *Transmitted_16()const;
+	const char *Transmitted_32()const;
+	const char *Compare_Bytes_8_CF_OF_SF_ZF_AF_PF()const;
+	const char *Compare_Bytes_16_CF_OF_SF_ZF_AF_PF()const;
+	const char *Compare_Bytes_32_CF_OF_SF_ZF_AF_PF()const;
+	const char *Compare_AX_Bytes_8_CF_OF_SF_ZF_AF_PF()const;
+	const char *Compare_AX_Bytes_16_CF_OF_SF_ZF_AF_PF()const;
+	const char *Compare_AX_Bytes_32_CF_OF_SF_ZF_AF_PF()const;
+	const char *Load_8_To_AL_From_DS_ESI()const;
+	const char *Load_16_To_AX_From_DS_ESI()const;
+	const char *Load_32_To_EAX_From_DS_ESI()const;
+	const char *Store_8_To_ES_EDI_From_AL()const;
+	const char *Store_16_To_ES_EDI_From_AX()const;
+	const char *Store_32_To_ES_EDI_From_EAX()const;
+	const char *Repeat_Prefix_ECX()const;
+	const char *Repeat_Prefix_EQUAL__ZF_ECX()const;
+	const char *Repeat_Prefix_NOT_EQUAL__ZF_ECX()const;
+	const char *Repeat_Prefix_CF_ECX()const;
+	const char *Repeat_Prefix_NOT_CF_ECX()const;
+};
+
+#endif
