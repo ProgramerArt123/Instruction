bd0d6bc2b0b81eb7e6ba424af41cc09e64e48177
ProgramerArt123 <2039231428@qq.com>
2022-06-22 22:18:18 +0800

    Compare

---
+++ x86/mnemonic/SIMD/MMX/32/Compare.cpp
@@ -0,0 +1,22 @@
+#include "Compare.h"
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace MMX
+			{
+				namespace size32
+				{
+					namespace compare {
+						const char *Compare_Packed_Bytes_64() {
+							return "PCMPEQB";
+						}
+						
+					}
+				}
+			}
+		}
+	}
+}
---
+++ x86/mnemonic/SIMD/MMX/32/Compare.h
@@ -0,0 +1,22 @@
+#ifndef __X86_MNEMONIC_SIMD_MMX_32_COMPARE_H__
+#define __X86_MNEMONIC_SIMD_MMX_32_COMPARE_H__
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace MMX
+			{
+				namespace size32
+				{
+					namespace compare {
+						const char *Compare_Packed_Bytes_64();
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
