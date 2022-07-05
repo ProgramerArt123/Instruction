f17fcf099b6e05c7a8519b7fd20141f2044f0d1b
ProgramerArt123 <2039231428@qq.com>
2022-07-05 20:39:05 +0800

    State

---
+++ x86/mnemonic/SIMD/MMX/32/State.cpp
@@ -0,0 +1,21 @@
+#include "State.h"
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace MMX
+			{
+				namespace size32
+				{
+					namespace state {
+						const char *Empty_State() {
+							return "EMMS";
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ x86/mnemonic/SIMD/MMX/32/State.h
@@ -0,0 +1,22 @@
+#ifndef __X86_MNEMONIC_SIMD_MMX_32_STATE_H__
+#define __X86_MNEMONIC_SIMD_MMX_32_STATE_H__
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace MMX
+			{
+				namespace size32
+				{
+					namespace logical {
+						const char *Empty_State();
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
