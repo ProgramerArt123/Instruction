62688c16ce715a09aab0265fd7fcc02c97dca372
ProgramerArt123 <2039231428@qq.com>
2022-07-05 21:00:33 +0800

    Move

---
+++ x86/mnemonic/SIMD/MMX/32/Move.cpp
@@ -0,0 +1,21 @@
+#include "Move.h"
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace MMX
+			{
+				namespace size32
+				{
+					namespace move {
+						const char *Move_Packed_Words_Left() {
+							return "PSLLW";
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ x86/mnemonic/SIMD/MMX/32/Move.h
@@ -0,0 +1,22 @@
+#ifndef __X86_MNEMONIC_SIMD_MMX_32_MOVE_H__
+#define __X86_MNEMONIC_SIMD_MMX_32_MOVE_H__
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace MMX
+			{
+				namespace size32
+				{
+					namespace move {
+						const char *Move_Packed_Words_Left();
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
