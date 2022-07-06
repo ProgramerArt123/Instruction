89f042ff8d7bb858a8314e93efd1fca3af288bf6
ProgramerArt123 <2039231428@qq.com>
2022-07-06 22:02:20 +0800

    Arithmetic

--- x86/mnemonic/SIMD/MMX/32/Arithmetic.cpp
+++ x86/mnemonic/SIMD/MMX/32/Arithmetic.cpp
@@ -11,14 +11,56 @@ namespace x86
 				{
 					namespace arithmetic
 					{
-						const char *Add_Packed_Bytes_64__128() {
+						const char *Add_Packed_Bytes_64() {
 							return "PADDB";
 						}
-						const char *Add_Packed_Words_64__128() {
+						const char *Add_Packed_Words_64() {
 							return "PADDW";
 						}
-						const char *Add_Packed_Doublewords_128() {
+						const char *Add_Packed_Doublewords_64() {
 							return "PADDD";
+						}
+						const char *Add_Packed_Signed_Bytes_64() {
+							return "PADDSB";
+						}
+						const char *Add_Packed_Signed_Words_64() {
+							return "PADDSW";
+						}
+						const char *Add_Packed_Unsigned_Bytes_64() {
+							return "PADDUSB";
+						}
+						const char *Add_Packed_Unsigned_Words_64() {
+							return "PADDUSW";
+						}
+						const char *Subtract_Packed_Bytes_64() {
+							return "PSUBB";
+						}
+						const char *Subtract_Packed_Words_64() {
+							return "PSUBW";
+						}
+						const char *Subtract_Packed_Doublewords_64() {
+							return "PSUBD";
+						}
+						const char *Subtract_Packed_Signed_Bytes_64() {
+							return "PSUBSB";
+						}
+						const char *Subtract_Packed_Signed_Words_64() {
+							return "PSUBSW";
+						}
+						const char *Subtract_Packed_Unsigned_Bytes_64() {
+							return "PSUBUSB";
+						}
+						const char *Subtract_Packed_Unsigned_Words_64() {
+							return "PSUBUSW";
+						}
+						const char *Multiply_Packed_Signed_Words_High_64() {
+							return "PMULHW";
+						}
+						const char *Multiply_Packed_Signed_Words_Low_64() {
+							return "PMULLW";
+						}
+						const char *Multiply_Add_Packed_Words_64() {
+							return "PMADDWD";
 						}
 					}
 				}
--- x86/mnemonic/SIMD/MMX/32/Arithmetic.h
+++ x86/mnemonic/SIMD/MMX/32/Arithmetic.h
@@ -1,5 +1,5 @@
-#ifndef __X86_MNEMONIC_SIMD_MMX_32_DATA_ARITHMETIC_H__
-#define __X86_MNEMONIC_SIMD_MMX_32_DATA_ARITHMETIC_H__
+#ifndef __X86_MNEMONIC_SIMD_MMX_32_ARITHMETIC_H__
+#define __X86_MNEMONIC_SIMD_MMX_32_ARITHMETIC_H__
 namespace x86
 {
 	namespace mnemonic 
@@ -12,9 +12,23 @@ namespace x86
 				{
 					namespace arithmetic
 					{
-						const char *Add_Packed_Bytes_64__128();
-						const char *Add_Packed_Words_64__128();
-						const char *Add_Packed_Doublewords_128();
+						const char *Add_Packed_Bytes_64();
+						const char *Add_Packed_Words_64();
+						const char *Add_Packed_Doublewords_64();
+						const char *Add_Packed_Signed_Bytes_64();
+						const char *Add_Packed_Signed_Words_64();
+						const char *Add_Packed_Unsigned_Bytes_64();
+						const char *Add_Packed_Unsigned_Words_64();
+						const char *Subtract_Packed_Bytes_64();
+						const char *Subtract_Packed_Words_64();
+						const char *Subtract_Packed_Doublewords_64();
+						const char *Subtract_Packed_Signed_Bytes_64();
+						const char *Subtract_Packed_Signed_Words_64();
+						const char *Subtract_Packed_Unsigned_Bytes_64();
+						const char *Subtract_Packed_Unsigned_Words_64();
+						const char *Multiply_Packed_Signed_Words_High_64();
+						const char *Multiply_Packed_Signed_Words_Low_64();
+						const char *Multiply_Add_Packed_Words_64();
 					}
 				}
 			}
