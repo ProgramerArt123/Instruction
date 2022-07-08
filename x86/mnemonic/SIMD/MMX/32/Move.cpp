25aa3d6b89fd1913da64b359bf87d7b556b3fc31
ProgramerArt123 <2039231428@qq.com>
2022-07-08 21:29:50 +0800

    Move

--- x86/mnemonic/SIMD/MMX/32/Move.cpp
+++ x86/mnemonic/SIMD/MMX/32/Move.cpp
@@ -10,8 +10,29 @@ namespace x86
 				namespace size32
 				{
 					namespace move {
-						const char *Move_Packed_Words_Left() {
+						const char *Move_Packed_Logical_Words_Left_64() {
 							return "PSLLW";
+						}
+						const char *Move_Packed_Logical_Doublewords_Left_64() {
+							return "PSLLD";
+						}
+						const char *Move_Packed_Logical_Quadword_Left_64() {
+							return "PSLLQ";
+						}
+						const char *Move_Packed_Logical_Words_Right_64() {
+							return "PSRLW";
+						}
+						const char *Move_Packed_Logical_Doublewords_Right_64() {
+							return "PSRLD";
+						}
+						const char *Move_Packed_Logical_Quadword_Right_64() {
+							return "PSRLQ";
+						}
+						const char *Move_Packed_Arithmetic_Words_Right_64() {
+							return "PSRAW";
+						}
+						const char *Move_Packed_Arithmetic_Doublewords_Right_64() {
+							return "PSRAD";
 						}
 					}
 				}
--- x86/mnemonic/SIMD/MMX/32/Move.h
+++ x86/mnemonic/SIMD/MMX/32/Move.h
@@ -11,7 +11,14 @@ namespace x86
 				namespace size32
 				{
 					namespace move {
-						const char *Move_Packed_Words_Left();
+						const char *Move_Packed_Logical_Words_Left_64();
+						const char *Move_Packed_Logical_Doublewords_Left_64();
+						const char *Move_Packed_Logical_Quadword_Left_64();
+						const char *Move_Packed_Logical_Words_Right_64();
+						const char *Move_Packed_Logical_Doublewords_Right_64();
+						const char *Move_Packed_Logical_Quadword_Right_64();
+						const char *Move_Packed_Arithmetic_Words_Right_64();
+						const char *Move_Packed_Arithmetic_Doublewords_Right_64();
 					}
 				}
 			}
