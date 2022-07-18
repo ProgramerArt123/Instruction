3ad4050e7c04ad385c83c8b1c3ac43468ff7ce9e
ProgramerArt123 <2039231428@qq.com>
2022-07-18 22:50:33 +0800

    SSE3.Shuffle

---
+++ x86/mnemonic/SIMD/SSE/SSE3/Shuffle.cpp
@@ -0,0 +1,22 @@
+#include "Shuffle.h"
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace SSE
+			{
+				namespace SSE3
+				{
+					namespace arithmetic
+					{
+						const char *Packed_Shuffle_Bytes_64__128() {
+							return "PSHUFB";
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ x86/mnemonic/SIMD/SSE/SSE3/Shuffle.h
@@ -0,0 +1,23 @@
+#ifndef __X86_MNEMONIC_SIMD_SSE_SSE3_SHUFFLE_H__
+#define __X86_MNEMONIC_SIMD_SSE_SSE3_SHUFFLE_H__
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace SSE
+			{
+				namespace SSE3
+				{
+					namespace shuffle
+					{
+						const char *Packed_Shuffle_Bytes_64__128();
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
