f2452785746bc99f5858a5fbb4f45b19c4b6a8cf
ProgramerArt123 <2039231428@qq.com>
2022-07-23 16:35:15 +0800

    SSE41.Compare

---
+++ x86/mnemonic/SIMD/SSE/SSE4/SSE4.1/Compare.cpp
@@ -0,0 +1,27 @@
+#include "Compare.h"
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace SSE
+			{
+				namespace SSE4
+				{
+					namespace SSE4_1
+					{
+						namespace compare {
+							const char *Compare_ZF_CF() {
+								return "PTEST";
+							}
+							const char *Compare_Packed_Quadwords() {
+								return "PCMPEQQ";
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ x86/mnemonic/SIMD/SSE/SSE4/SSE4.1/Compare.h
@@ -0,0 +1,26 @@
+#ifndef __X86_MNEMONIC_SIMD_SSE_SSE4_SSE4_1_COMPARE_H__
+#define __X86_MNEMONIC_SIMD_SSE_SSE4_SSE4_1_COMPARE_H__
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace SSE
+			{
+				namespace SSE4
+				{
+					namespace SSE4_1
+					{
+						namespace compare {
+							const char *Compare_ZF_CF();
+							const char *Compare_Packed_Quadwords();
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
