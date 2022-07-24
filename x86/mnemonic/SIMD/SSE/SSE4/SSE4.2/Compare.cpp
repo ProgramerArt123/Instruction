a3951eef20ced70d7d33381dc88069464e3a5cae
ProgramerArt123 <2039231428@qq.com>
2022-07-24 14:58:15 +0800

    SSE42.Compare

---
+++ x86/mnemonic/SIMD/SSE/SSE4/SSE4.2/Compare.cpp
@@ -0,0 +1,24 @@
+#include "Compare.h"
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace SSE
+			{
+				namespace SSE4
+				{
+					namespace SSE4_2
+					{
+						namespace compare {
+							const char *Compare_Packed_Signed_Quadwords_128() {
+								return "PCMPGTQ";
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ x86/mnemonic/SIMD/SSE/SSE4/SSE4.2/Compare.h
@@ -0,0 +1,25 @@
+#ifndef __X86_MNEMONIC_SIMD_SSE_SSE4_SSE4_2_COMPARE_H__
+#define __X86_MNEMONIC_SIMD_SSE_SSE4_SSE4_2_COMPARE_H__
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace SSE
+			{
+				namespace SSE4
+				{
+					namespace SSE4_2
+					{
+						namespace compare {
+							const char *Compare_Packed_Signed_Quadwords_128();
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
