3f8ab43d8eadb39cfce068c930d77579f3b44cb3
ProgramerArt123 <2039231428@qq.com>
2022-07-24 16:36:28 +0800

    SSE42.Arithmetic

---
+++ x86/mnemonic/SIMD/SSE/SSE4/SSE4.2/Arithmetic.cpp
@@ -0,0 +1,25 @@
+#include "Arithmetic.h"
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace SSE
+			{
+				namespace SSE4
+				{
+					namespace SSE4_2
+					{
+						namespace arithmetic
+						{
+							const char *Carry_Less_Multiplication_Quadwords_128() {
+								return "PCLMULQDQ";
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ x86/mnemonic/SIMD/SSE/SSE4/SSE4.2/Arithmetic.h
@@ -0,0 +1,26 @@
+#ifndef __X86_MNEMONIC_SIMD_SSE_SSE4_SSE4_2_ARITHMETIC_H__
+#define __X86_MNEMONIC_SIMD_SSE_SSE4_SSE4_2_ARITHMETIC_H__
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace SSE
+			{
+				namespace SSE4
+				{
+					namespace SSE4_2
+					{
+						namespace arithmetic
+						{
+							const char *Carry_Less_Multiplication_Quadwords_128();
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
