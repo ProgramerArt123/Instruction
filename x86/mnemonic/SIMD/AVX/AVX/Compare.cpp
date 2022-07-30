d9b53b0b1067a14065198c0f02b105faa506dfbf
ProgramerArt123 <2039231428@qq.com>
2022-07-30 18:56:34 +0800

    AVX Compare

---
+++ x86/mnemonic/SIMD/AVX/AVX/Compare.cpp
@@ -0,0 +1,24 @@
+#include "Compare.h"
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace AVX
+			{
+				namespace AVX
+				{
+					namespace compare {
+						const char *Compare_Packed_Single_128__256() {
+							return "VCMPPS";
+						}
+						const char *Compare_Packed_Double_128__256() {
+							return "VCMPPD";
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ x86/mnemonic/SIMD/AVX/AVX/Compare.h
@@ -0,0 +1,23 @@
+#ifndef __X86_MNEMONIC_SIMD_AVX_AVX_COMPARE_H__
+#define __X86_MNEMONIC_SIMD_AVX_AVX_COMPARE_H__
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace AVX
+			{
+				namespace AVX
+				{
+					namespace compare {
+						const char *Compare_Packed_Single_128__256();
+						const char *Compare_Packed_Double_128__256();
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
