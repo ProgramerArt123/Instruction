6e8c3a7083330b20463a3aff39f9c89c78bd42aa
ProgramerArt123 <2039231428@qq.com>
2022-07-30 18:51:18 +0800

    AVX Conversion

--- x86/mnemonic/SIMD/AVX/AVX/Conversion.cpp
+++ x86/mnemonic/SIMD/AVX/AVX/Conversion.cpp
@@ -16,7 +16,24 @@ namespace x86
 						const char *Convert_Packed_Double_Packed_Single_128__256() {
 							return "VCVTPD2PS";
 						}
-						
+						const char *Convert_Packed_Doubleword_Packed_Single_128__256() {
+							return "VCVTDQ2PS";
+						}
+						const char *Convert_Packed_Single_Packed_Doubleword_128__256() {
+							return "VCVTPS2DQ";
+						}
+						const char *Convert_Truncation_Packed_Single_Packed_Signed_Doubleword_128__256() {
+							return "VCVTTPS2DQ";
+						}
+						const char *Convert_Truncation_Packed_Double_Packed_Doubleword_128__256() {
+							return "VCVTTPD2DQ";
+						}
+						const char *Convert_Packed_Double_Packed_Doubleword_128__256() {
+							return "VCVTPD2DQ";
+						}
+						const char *Convert_Packed_Doubleword_Packed_Double_64__128() {
+							return "VCVTDQ2PD";
+						}
 					}
 				}
 			}
--- x86/mnemonic/SIMD/AVX/AVX/Conversion.h
+++ x86/mnemonic/SIMD/AVX/AVX/Conversion.h
@@ -13,7 +13,12 @@ namespace x86
 					namespace convertion {
 						const char *Convert_Packed_Single_Packed_Double_64__128();
 						const char *Convert_Packed_Double_Packed_Single_128__256();
-						
+						const char *Convert_Packed_Doubleword_Packed_Single_128__256();
+						const char *Convert_Packed_Single_Packed_Doubleword_128__256();	
+						const char *Convert_Truncation_Packed_Single_Packed_Signed_Doubleword_128__256();
+						const char *Convert_Truncation_Packed_Double_Packed_Doubleword_128__256();
+						const char *Convert_Packed_Double_Packed_Doubleword_128__256();
+						const char *Convert_Packed_Doubleword_Packed_Double_64__128();
 					}
 				}
 			}
