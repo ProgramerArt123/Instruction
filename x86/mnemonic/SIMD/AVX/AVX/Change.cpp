3f47ef8682eca61503ee1837e3b75034ed645c05
ProgramerArt123 <2039231428@qq.com>
2022-07-31 15:19:36 +0800

    AVX Change

---
+++ x86/mnemonic/SIMD/AVX/AVX/Change.cpp
@@ -0,0 +1,27 @@
+#include "Change.h"
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace AVX
+			{
+				namespace AVX
+				{
+					namespace change {
+						const char *Packed_Interleave_Shuffle_Two_Double_128__256() {
+							return "VSHUFPD";
+						}
+						const char *Packed_Interleave_Shuffle_Quadruplets_Single_128__256() {
+							return "VSHUFPS";
+						}
+						const char *Unpack_Interleave_Low_Packed_Single_128__256() {
+							return "VUNPCKLPS";
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ x86/mnemonic/SIMD/AVX/AVX/Change.h
@@ -0,0 +1,24 @@
+#ifndef __X86_MNEMONIC_SIMD_AVX_AVX_CHANGE_H__
+#define __X86_MNEMONIC_SIMD_AVX_AVX_CHANGE_H__
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace AVX
+			{
+				namespace AVX
+				{
+					namespace change {
+						const char *Packed_Interleave_Shuffle_Two_Double_128__256();
+						const char *Packed_Interleave_Shuffle_Quadruplets_Single_128__256();
+						const char *Unpack_Interleave_Low_Packed_Single_128__256();
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
