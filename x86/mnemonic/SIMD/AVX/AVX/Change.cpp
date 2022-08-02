5ad7c0398b70fedbbd514dca3d754e79b984d905
ProgramerArt123 <2039231428@qq.com>
2022-08-02 23:06:07 +0800

    AVX Change

--- x86/mnemonic/SIMD/AVX/AVX/Change.cpp
+++ x86/mnemonic/SIMD/AVX/AVX/Change.cpp
@@ -19,6 +19,15 @@ namespace x86
 						const char *Unpack_Interleave_Low_Packed_Single_128__256() {
 							return "VUNPCKLPS";
 						}
+						const char *Permute_Double_128__256() {
+							return "VPERMILPD";
+						}
+						const char *Permute_Single_128__256() {
+							return "VPERMILPS";
+						}
+						const char *Permute_Float_256() {
+							return "VPERM2F128";
+						}
 					}
 				}
 			}
--- x86/mnemonic/SIMD/AVX/AVX/Change.h
+++ x86/mnemonic/SIMD/AVX/AVX/Change.h
@@ -14,6 +14,9 @@ namespace x86
 						const char *Packed_Interleave_Shuffle_Two_Double_128__256();
 						const char *Packed_Interleave_Shuffle_Quadruplets_Single_128__256();
 						const char *Unpack_Interleave_Low_Packed_Single_128__256();
+						const char *Permute_Double_128__256();
+						const char *Permute_Single_128__256();
+						const char *Permute_Float_256();
 					}
 				}
 			}
