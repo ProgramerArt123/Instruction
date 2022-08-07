d03eccaaa0158a18cf6ee87cc1efc3df587a2ca4
ProgramerArt123 <2039231428@qq.com>
2022-08-07 09:31:48 +0800

    AVX 64 Arithmetic

---
+++ x86/mnemonic/SIMD/AVX/AVX/64/Arithmetic.cpp
@@ -0,0 +1,26 @@
+#include "Arithmetic.h"
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace AVX
+			{
+				namespace AVX
+				{
+					namespace size64
+					{
+						namespace arithmetic
+						{
+							const char *Return_Count_Number_Bits_One_16__32__64() {
+								return "POPCNT";
+							}
+							
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ x86/mnemonic/SIMD/AVX/AVX/64/Arithmetic.h
@@ -0,0 +1,26 @@
+#ifndef __X86_MNEMONIC_SIMD_AVX_AVX_64_ARITHMETIC_H__
+#define __X86_MNEMONIC_SIMD_AVX_AVX_64_ARITHMETIC_H__
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace AVX
+			{
+				namespace AVX
+				{
+					namespace size64
+					{
+						namespace arithmetic
+						{
+							const char *Return_Count_Number_Bits_One_16__32__64();
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
