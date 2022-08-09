b20fd4e3ba7964b5a5650409729ed6329412e3d9
ProgramerArt123 <2039231428@qq.com>
2022-08-09 19:15:26 +0800

    AVX FMA

---
+++ x86/mnemonic/SIMD/AVX/AVX/FMA/Arithmetic.cpp
@@ -0,0 +1,202 @@
+#include "Arithmetic.h"
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace AVX
+			{
+				namespace AVX
+				{
+					namespace FMA
+					{
+						namespace arithmetic
+						{
+							const char *Multiply_Add_Packed_Double_1_3_2() {
+								return "VFMADD132PD";
+							}
+							const char *Multiply_Add_Packed_Double_2_1_3() {
+								return "VFMADD213PD";
+							}
+							const char *Multiply_Add_Packed_Double_2_3_1() {
+								return "VFMADD231PD";
+							}
+							const char *Multiply_Add_Packed_Single_1_3_2() {
+								return "VFMADD132PS";
+							}
+							const char *Multiply_Add_Packed_Single_2_1_3() {
+								return "VFMADD213PS";
+							}
+							const char *Multiply_Add_Packed_Single_2_3_1() {
+								return "VFMADD231PS";
+							}
+							const char *Multiply_Add_Scalar_Double_1_3_2() {
+								return "VFMADD132SD";
+							}
+							const char *Multiply_Add_Scalar_Double_2_1_3() {
+								return "VFMADD213SD";
+							}
+							const char *Multiply_Add_Scalar_Double_2_3_1() {
+								return "VFMADD231SD";
+							}
+							const char *Multiply_Add_Scalar_Single_1_3_2() {
+								return "VFMADD132SS";
+							}
+							const char *Multiply_Add_Scalar_Single_2_1_3() {
+								return "VFMADD213SS";
+							}
+							const char *Multiply_Add_Scalar_Single_2_3_1() {
+								return "VFMADD231SS";
+							}
+							const char *Multiply_Alternating_Add__Subtract_Packed_Double_1_3_2() {
+								return "VFMADDSUB132PD";
+							}
+							const char *Multiply_Alternating_Add__Subtract_Packed_Double_2_1_3() {
+								return "VFMADDSUB213PD";
+							}
+							const char *Multiply_Alternating_Add__Subtract_Packed_Double_2_3_1() {
+								return "VFMADDSUB231PD";
+							}
+							const char *Multiply_Alternating_Add__Subtract_Packed_Single_1_3_2() {
+								return "VFMADDSUB132PS";
+							}
+							const char *Multiply_Alternating_Add__Subtract_Packed_Single_2_1_3() {
+								return "VFMADDSUB213PS";
+							}
+							const char *Multiply_Alternating_Add__Subtract_Packed_Single_2_3_1() {
+								return "VFMADDSUB231PS";
+							}
+							const char *Multiply_Alternating_Subtract__Add_Packed_Double_1_3_2() {
+								return "VFMSUBADD132PD";
+							}
+							const char *Multiply_Alternating_Subtract__Add_Packed_Double_2_1_3() {
+								return "VFMSUBADD213PD";
+							}
+							const char *Multiply_Alternating_Subtract__Add_Packed_Double_2_3_1() {
+								return "VFMSUBADD231PD";
+							}
+							const char *Multiply_Alternating_Subtract__Add_Packed_Single_1_3_2() {
+								return "VFMSUBADD132PS";
+							}
+							const char *Multiply_Alternating_Subtract__Add_Packed_Single_2_1_3() {
+								return "VFMSUBADD213PS";
+							}
+							const char *Multiply_Alternating_Subtract__Add_Packed_Single_2_3_1() {
+								return "VFMSUBADD231PS";
+							}
+							const char *Multiply_Subtract_Packed_Double_1_3_2() {
+								return "VFMSUB132PD";
+							}
+							const char *Multiply_Subtract_Packed_Double_2_1_3() {
+								return "VFMSUB213PD";
+							}
+							const char *Multiply_Subtract_Packed_Double_2_3_1() {
+								return "VFMSUB231PD";
+							}
+							const char *Multiply_Subtract_Packed_Single_1_3_2() {
+								return "VFMSUB132PS";
+							}
+							const char *Multiply_Subtract_Packed_Single_2_1_3() {
+								return "VFMSUB213PS";
+							}
+							const char *Multiply_Subtract_Packed_Single_2_3_1() {
+								return "VFMSUB231PS";
+							}
+							const char *Multiply_Subtract_Scalar_Double_1_3_2() {
+								return "VFMSUB132SD";
+							}
+							const char *Multiply_Subtract_Scalar_Double_2_1_3() {
+								return "VFMSUB213SD";
+							}
+							const char *Multiply_Subtract_Scalar_Double_2_3_1() {
+								return "VFMSUB231SD";
+							}
+							const char *Multiply_Subtract_Scalar_Single_1_3_2() {
+								return "VFMSUB132SS";
+							}
+							const char *Multiply_Subtract_Scalar_Single_2_1_3() {
+								return "VFMSUB213SS";
+							}
+							const char *Multiply_Subtract_Scalar_Single_2_3_1() {
+								return "VFMSUB231SS";
+							}
+							const char *Negative_Multiply_Add_Packed_Double_1_3_2() {
+								return "VFNMADD132PD";
+							}
+							const char *Negative_Multiply_Add_Packed_Double_2_1_3() {
+								return "VFNMADD213PD";
+							}
+							const char *Negative_Multiply_Add_Packed_Double_2_3_1() {
+								return "VFNMADD231PD";
+							}
+							const char *Negative_Multiply_Add_Packed_Single_1_3_2() {
+								return "VFNMADD132PS";
+							}
+							const char *Negative_Multiply_Add_Packed_Single_2_1_3() {
+								return "VFNMADD213PS";
+							}
+							const char *Negative_Multiply_Add_Packed_Single_2_3_1() {
+								return "VFNMADD231PS";
+							}
+							const char *Negative_Multiply_Add_Scalar_Double_1_3_2() {
+								return "VFNMADD132SD";
+							}
+							const char *Negative_Multiply_Add_Scalar_Double_2_1_3() {
+								return "VFNMADD213SD";
+							}
+							const char *Negative_Multiply_Add_Scalar_Double_2_3_1() {
+								return "VFNMADD231SD";
+							}
+							const char *Negative_Multiply_Add_Scalar_Single_1_3_2() {
+								return "VFNMADD132SS";
+							}
+							const char *Negative_Multiply_Add_Scalar_Single_2_1_3() {
+								return "VFNMADD213SS";
+							}
+							const char *Negative_Multiply_Add_Scalar_Single_2_3_1() {
+								return "VFNMADD231SS";
+							}
+							const char *Negative_Multiply_Subtract_Packed_Double_1_3_2() {
+								return "VFNMSUB132PD";
+							}
+							const char *Negative_Multiply_Subtract_Packed_Double_2_1_3() {
+								return "VFNMSUB213PD";
+							}
+							const char *Negative_Multiply_Subtract_Packed_Double_2_3_1() {
+								return "VFNMSUB231PD";
+							}
+							const char *Negative_Multiply_Subtract_Packed_Single_1_3_2() {
+								return "VFNMSUB132PS";
+							}
+							const char *Negative_Multiply_Subtract_Packed_Single_2_1_3() {
+								return "VFNMSUB213PS";
+							}
+							const char *Negative_Multiply_Subtract_Packed_Single_2_3_1() {
+								return "VFNMSUB231PS";
+							}
+							const char *Negative_Multiply_Subtract_Scalar_Double_1_3_2() {
+								return "VFNMSUB132SD";
+							}
+							const char *Negative_Multiply_Subtract_Scalar_Double_2_1_3() {
+								return "VFNMSUB213SD";
+							}
+							const char *Negative_Multiply_Subtract_Scalar_Double_2_3_1() {
+								return "VFNMSUB231SD";
+							}
+							const char *Negative_Multiply_Subtract_Scalar_Single_1_3_2() {
+								return "VFNMSUB132SS";
+							}
+							const char *Negative_Multiply_Subtract_Scalar_Single_2_1_3() {
+								return "VFNMSUB213SS";
+							}
+							const char *Negative_Multiply_Subtract_Scalar_Single_2_3_1() {
+								return "VFNMSUB231SS";
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ x86/mnemonic/SIMD/AVX/AVX/FMA/Arithmetic.h
@@ -0,0 +1,85 @@
+#ifndef __X86_MNEMONIC_SIMD_AVX_AVX_FMA_ARITHMETIC_H__
+#define __X86_MNEMONIC_SIMD_AVX_AVX_FMA_ARITHMETIC_H__
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace AVX
+			{
+				namespace AVX
+				{
+					namespace FMA
+					{
+						namespace arithmetic
+						{
+							const char *Multiply_Add_Packed_Double_1_3_2();
+							const char *Multiply_Add_Packed_Double_2_1_3();
+							const char *Multiply_Add_Packed_Double_2_3_1();
+							const char *Multiply_Add_Packed_Single_1_3_2();
+							const char *Multiply_Add_Packed_Single_2_1_3();
+							const char *Multiply_Add_Packed_Single_2_3_1();
+							const char *Multiply_Add_Scalar_Double_1_3_2();
+							const char *Multiply_Add_Scalar_Double_2_1_3();
+							const char *Multiply_Add_Scalar_Double_2_3_1();
+							const char *Multiply_Add_Scalar_Single_1_3_2();
+							const char *Multiply_Add_Scalar_Single_2_1_3();
+							const char *Multiply_Add_Scalar_Single_2_3_1();
+							const char *Multiply_Alternating_Add__Subtract_Packed_Double_1_3_2();
+							const char *Multiply_Alternating_Add__Subtract_Packed_Double_2_1_3();
+							const char *Multiply_Alternating_Add__Subtract_Packed_Double_2_3_1();
+							const char *Multiply_Alternating_Add__Subtract_Packed_Single_1_3_2();
+							const char *Multiply_Alternating_Add__Subtract_Packed_Single_2_1_3();
+							const char *Multiply_Alternating_Add__Subtract_Packed_Single_2_3_1();
+							const char *Multiply_Alternating_Subtract__Add_Packed_Double_1_3_2();
+							const char *Multiply_Alternating_Subtract__Add_Packed_Double_2_1_3();
+							const char *Multiply_Alternating_Subtract__Add_Packed_Double_2_3_1();
+							const char *Multiply_Alternating_Subtract__Add_Packed_Single_1_3_2();
+							const char *Multiply_Alternating_Subtract__Add_Packed_Single_2_1_3();
+							const char *Multiply_Alternating_Subtract__Add_Packed_Single_2_3_1();
+							const char *Multiply_Subtract_Packed_Double_1_3_2();
+							const char *Multiply_Subtract_Packed_Double_2_1_3();
+							const char *Multiply_Subtract_Packed_Double_2_3_1();
+							const char *Multiply_Subtract_Packed_Single_1_3_2();
+							const char *Multiply_Subtract_Packed_Single_2_1_3();
+							const char *Multiply_Subtract_Packed_Single_2_3_1();
+							const char *Multiply_Subtract_Scalar_Double_1_3_2();
+							const char *Multiply_Subtract_Scalar_Double_2_1_3();
+							const char *Multiply_Subtract_Scalar_Double_2_3_1();
+							const char *Multiply_Subtract_Scalar_Single_1_3_2();
+							const char *Multiply_Subtract_Scalar_Single_2_1_3();
+							const char *Multiply_Subtract_Scalar_Single_2_3_1();
+							const char *Negative_Multiply_Add_Packed_Double_1_3_2();
+							const char *Negative_Multiply_Add_Packed_Double_2_1_3();
+							const char *Negative_Multiply_Add_Packed_Double_2_3_1();
+							const char *Negative_Multiply_Add_Packed_Single_1_3_2();
+							const char *Negative_Multiply_Add_Packed_Single_2_1_3();
+							const char *Negative_Multiply_Add_Packed_Single_2_3_1();
+							const char *Negative_Multiply_Add_Scalar_Double_1_3_2();
+							const char *Negative_Multiply_Add_Scalar_Double_2_1_3();
+							const char *Negative_Multiply_Add_Scalar_Double_2_3_1();
+							const char *Negative_Multiply_Add_Scalar_Single_1_3_2();
+							const char *Negative_Multiply_Add_Scalar_Single_2_1_3();
+							const char *Negative_Multiply_Add_Scalar_Single_2_3_1();
+							const char *Negative_Multiply_Subtract_Packed_Double_1_3_2();
+							const char *Negative_Multiply_Subtract_Packed_Double_2_1_3();
+							const char *Negative_Multiply_Subtract_Packed_Double_2_3_1();
+							const char *Negative_Multiply_Subtract_Packed_Single_1_3_2();
+							const char *Negative_Multiply_Subtract_Packed_Single_2_1_3();
+							const char *Negative_Multiply_Subtract_Packed_Single_2_3_1();
+							const char *Negative_Multiply_Subtract_Scalar_Double_1_3_2();
+							const char *Negative_Multiply_Subtract_Scalar_Double_2_1_3();
+							const char *Negative_Multiply_Subtract_Scalar_Double_2_3_1();
+							const char *Negative_Multiply_Subtract_Scalar_Single_1_3_2();
+							const char *Negative_Multiply_Subtract_Scalar_Single_2_1_3();
+							const char *Negative_Multiply_Subtract_Scalar_Single_2_3_1();
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
