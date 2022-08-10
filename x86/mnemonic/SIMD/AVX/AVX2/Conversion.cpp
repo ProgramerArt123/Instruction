ddf4a7b33fdba2c81b138ad6a3d9972ab2d06017
ProgramerArt123 <2039231428@qq.com>
2022-08-10 11:28:12 +0800

    AVX2 Conversion

--- x86/mnemonic/SIMD/AVX/AVX2/Conversion.cpp
+++ x86/mnemonic/SIMD/AVX/AVX2/Conversion.cpp
@@ -25,6 +25,33 @@ namespace x86
 						const char *Pack_Words_Into_Bytes_Signed_256() {
 							return "VPACKSSWB";
 						}
+						const char *Pack_Words_Into_Bytes_Unsigned_256() {
+							return "VPACKUSWB";
+						}
+						const char *Unpack_High_Order_Bytes_256() {
+							return "VPUNPCKHBW";
+						}
+						const char *Unpack_High_Order_Words_256() {
+							return "VPUNPCKHWD";
+						}
+						const char *Unpack_High_Order_Doublewords_256() {
+							return "VPUNPCKHDQ";
+						}
+						const char *Unpack_High_Quadwords_256() {
+							return "VPUNPCKHQDQ";
+						}
+						const char *Pack_Doublewords_Into_Words_Signed_256() {
+							return "VPACKSSDW";
+						}
+						const char *Shuffle_Packed_Doublewords_256() {
+							return "VPSHUFD";
+						}
+						const char *Shuffle_Packed_Low_Words_256() {
+							return "VPSHUFLW";
+						}
+						const char *Shuffle_Packed_High_Words_256() {
+							return "VPSHUFHW";
+						}
 					}
 				}
 			}
--- x86/mnemonic/SIMD/AVX/AVX2/Conversion.h
+++ x86/mnemonic/SIMD/AVX/AVX2/Conversion.h
@@ -16,6 +16,15 @@ namespace x86
 						const char *Unpack_Low_Order_Doublewords_256();
 						const char *Unpack_Low_Order_Quadwords_256();
 						const char *Pack_Words_Into_Bytes_Signed_256();
+						const char *Pack_Words_Into_Bytes_Unsigned_256();
+						const char *Unpack_High_Order_Bytes_256();
+						const char *Unpack_High_Order_Words_256();
+						const char *Unpack_High_Order_Doublewords_256();
+						const char *Unpack_High_Quadwords_256();
+						const char *Pack_Doublewords_Into_Words_Signed_256();
+						const char *Shuffle_Packed_Doublewords_256();
+						const char *Shuffle_Packed_Low_Words_256(); 
+						const char *Shuffle_Packed_High_Words_256();
 					}
 				}
 			}
