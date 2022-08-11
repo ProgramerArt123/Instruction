05e517a959090f1cd56de9d2f8e7bd5689b4d403
ProgramerArt123 <2039231428@qq.com>
2022-08-11 14:45:11 +0800

    AVX2 Logical

---
+++ x86/mnemonic/SIMD/AVX/AVX2/Logical.cpp
@@ -0,0 +1,21 @@
+#include "Logical.h"
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace AVX
+			{
+				namespace AVX2
+				{
+					namespace logical {
+						const char *Logical_AND_256() {
+							return "VPAND";
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ x86/mnemonic/SIMD/AVX/AVX2/Logical.h
@@ -0,0 +1,22 @@
+#ifndef __X86_MNEMONIC_SIMD_AVX_AVX2_LOGICAL_H__
+#define __X86_MNEMONIC_SIMD_AVX_AVX2_LOGICAL_H__
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace AVX
+			{
+				namespace AVX2
+				{
+					namespace logical {
+						const char *Logical_AND_256();
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
