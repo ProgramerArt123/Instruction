4643370c3130ed3eba560ed96ac9ac09ac643a7a
ProgramerArt123 <2039231428@qq.com>
2022-08-11 22:13:25 +0800

    AVX512 DataTransmitted

---
+++ x86/mnemonic/SIMD/AVX/AVX512/DataTransmitted.cpp
@@ -0,0 +1,22 @@
+#include "DataTransmitted.h"
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace AVX
+			{
+				namespace AVX512
+				{
+					namespace data_transmitted 
+					{
+						const char *Align_Doubleword_Vectors_128__256__512() {
+							return "VALIGND";
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ x86/mnemonic/SIMD/AVX/AVX512/DataTransmitted.h
@@ -0,0 +1,23 @@
+#ifndef __X86_MNEMONIC_SIMD_AVX_AVX512_DATA_TRANSMITTED_H__
+#define __X86_MNEMONIC_SIMD_AVX_AVX512_DATA_TRANSMITTED_H__
+namespace x86
+{
+	namespace mnemonic 
+	{
+		namespace SIMD
+		{
+			namespace AVX
+			{
+				namespace AVX512
+				{
+					namespace data_transmitted 
+					{
+						const char *Align_Doubleword_Vectors_128__256__512();
+					}
+				}
+			}
+		}
+	}
+}
+
+#endif
